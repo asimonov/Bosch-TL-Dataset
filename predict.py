3ecfc81efde4f9f3989506d359abda67b92139e4
Alexey <alexey.simonov@gmail.com>
2017-09-01 15:06:33 +0100

    prediction test script changed

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ from tl_classifier_cnn import TLClassifierCNN, TLLabelConverter
 
 # load data
 desired_dim = (32,32)
-data_dirs = ['data/tl-extract-train', 'data/tl-extract-test', 'data/tl-extract-additional']
+data_dirs = ['data/tl-extract-test']
 x, y = load_tl_extracts(data_dirs, desired_dim)
 # x is image in OpenCV imread format. pixels are uint8 from 0 to 255. shape is H, W, C. C is ordered BGR
 # y here are strings like 'green' etc
@@ -27,5 +27,10 @@ tlc.restore_checkpoint()
 batch_size = 50
 
 labels, probs = tlc.predict(x, batch_size=batch_size)
+
+correct = sum([1 if y[i]==labels[i] else 0 for i in range(len(y))])
+accuracy = correct / len(y)
+
+print('accuracy: {}. correct {} out of {}'.format(accuracy, correct, len(y)))
 
 tlc.close_session()
